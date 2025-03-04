e91d7543ec1e2e29492c5d1b0e616401c26172cf
devulaar-uc <devulaar@mail.uc.edu>
2025-03-04 12:32:30 -0500

    Update users.js
    Added Comment in routes/users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
+  // Will get response when /users route hits
   res.send('respond with a resource')
 })
 
