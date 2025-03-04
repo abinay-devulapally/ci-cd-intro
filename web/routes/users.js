1e259c0d790b9b1fadda706d662a4709a5d75071
devulaar-uc <devulaar@mail.uc.edu>
2025-03-04 12:52:30 -0500

    Update users.js
    test console logging

--- web/routes/users.js
+++ web/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 /* GET users listing. */
 router.get('/users', function (req, res, next) {
+  console.log("THIS IS TEST LOGGING")
   res.send('respond with a resource')
 })
 
